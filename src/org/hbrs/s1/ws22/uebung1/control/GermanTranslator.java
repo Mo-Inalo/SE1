2270fe4d5b7c0b875cee84585c3c3845cae5eeb8
minalo2s <minalo2s@smail.inf.h-brs.de>
2022-10-11 06:47:31 +0200

    Entwicklung von weiteren Test Klasse

--- src/org/hbrs/s1/ws22/uebung1/control/GermanTranslator.java
+++ src/org/hbrs/s1/ws22/uebung1/control/GermanTranslator.java
@@ -1,5 +1,6 @@
 package org.hbrs.s1.ws22.uebung1.control;
 
+
 public class GermanTranslator implements Translator {
 
 	public String date = "Okt/2022"; // Default-Wert
@@ -7,9 +8,17 @@ public class GermanTranslator implements Translator {
 	/**
 	 * Methode zur Übersetzung einer Zahl in eine String-Repraesentation
 	 */
-	public String translateNumber(int number) {
-		String [] feld ={"eins","zwei","drei","vier","fuenf","sechse","sieben","acht","neun","Zehn"};
-		return feld[number-1];
+	public String translateNumber(int number) throws IndexOutOfBoundsException {
+		try {
+			String [] feld ={"eins","zwei","drei","vier","fuenf","sechs","sieben","acht","neun","Zehn"};
+			return feld[number-1];
+		} catch (IndexOutOfBoundsException e) {
+			e.getMessage();
+			System.out.println("Übersetzung der Zahl ["+ number+"] nicht moeglich ! ");
+			System.out.println("Versionsnummer der Translator: " +Translator.version);
+			System.out.println(e);
+		}
+		return null;
 	}
 
 	/**
--- src/org/hbrs/s1/ws22/uebung1/tests/GermanTranslatorTest.java
+++ src/org/hbrs/s1/ws22/uebung1/tests/GermanTranslatorTest.java
@@ -1,13 +1,15 @@
-
 package org.hbrs.s1.ws22.uebung1.tests;
-
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.hbrs.s1.ws22.uebung1.control.GermanTranslator;
 import org.hbrs.s1.ws22.uebung1.control.Translator;
 import org.hbrs.s1.ws22.uebung1.control.TranslatorFactory;
 import org.junit.Test;
+
+
+ 
+
 
 
 public class GermanTranslatorTest {
@@ -16,7 +18,17 @@ public class GermanTranslatorTest {
     @Test
     public void germanTranlatorTest(){
         Translator translator = TranslatorFactory.creatGermanTranslator();
-        assertEquals("sieben", translator.translateNumber(7));
+        assertEquals("sechs", translator.translateNumber(6));
 
-}
+    }
+
+   /* @Test
+    public void germanTranlatorTest2() throws IndexOutOfBoundsException {
+        Translator translator = TranslatorFactory.creatGermanTranslator();
+        IndexOutOfBoundsException e = null;
+        assertThrows(e.getMessage(), translator.translateNumber(14));
+
+    }*/
+
+
 }
